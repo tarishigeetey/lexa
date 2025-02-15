ea6ac401b4e81f2e503ae4d7fe41d7e5a834ff35
Tarishi Geetey <tarishi.geetey@gmail.com>
2025-02-15 17:08:25 +0530

    update : backend url

--- lexa-frontend/src/services/apiService.js
+++ lexa-frontend/src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8002';
+const API_BASE_URL = process.env.BACKEND_URL;
 
 export const startConversationWithAudio = async (scenarioName, audioFile) => {
   try {
